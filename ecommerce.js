4911cef3f759da9340b3bf3b23382fe816719b61
RoukayaBechri <bechri.roukaya@gmail.com>
2018-10-28 17:09:04 +0100

    view profile page and some modification

--- ecommerce.js
+++ ecommerce.js
@@ -1,12 +1,13 @@
 function Inscription() {
     var nom = document.getElementById("firstname").value;
-    var lastname=document.getElementById("lastname").value;
+    var lastname = document.getElementById("lastname").value;
     var tel = document.getElementById("tel").value;
     var solde = document.getElementById("solde").value;
     var adresse = document.getElementById("adresse").value;
     var age = document.getElementById("age").value;
     var email = document.getElementById("email").value;
-    var password = document.getElementById("pass").value;
+    var password = document.getElementById("password").value;
+    console.log(password);
 
     var tabOfClient = JSON.parse(localStorage.getItem("tabOfClient"));
     if (tabOfClient == null) {
@@ -29,8 +30,9 @@ function Inscription() {
 
     tabOfClient.push(client);
     tabOfClient = localStorage.setItem("tabOfClient", JSON.stringify(tabOfClient));
-    alert("Your account created successfuly!");}
-   
+    alert("Your account created successfuly!");
+}
+
 
 function generateUid() {
     var uniqueId = Math.round(Math.floor(Math.random() * Math.floor(1000)));
@@ -51,11 +53,11 @@ function myFunction() {
     var age = document.getElementById("age").value;
     if (age < 18) {
         document.getElementById("outputAge").innerHTML = "age must be >18!!!";
-        return false;
+
     }
-    else{
-        document.getElementById("outputAge").setAttribute("style","display: none");
-        return true;
+    else {
+        document.getElementById("outputAge").setAttribute("style", "display: none");
+
     }
 }
 
@@ -68,48 +70,89 @@ function myTest() {
     if (!tel.match(regex)) {
 
         document.getElementById("outputTel").innerHTML = "Please provide a valid phone number";
-        return false;
+
     }
-    else{
-        document.getElementById("outputTel").setAttribute("style","display: none");
-        return true;
+    else {
+        document.getElementById("outputTel").setAttribute("style", "display: none");
+
     }
 
 }
 
 
 
-function TestEmail() { 
-    document.getElementById("email").addEventListener("blur",emailTest());
- }
+function TestEmail() {
+    document.getElementById("email").addEventListener("blur", emailTest());
+}
 function emailTest() {
-    var email=document.getElementById("email").value;
+    var email = document.getElementById("email").value;
     var regexmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!email.match(regexmail)) {
         document.getElementById("outputEmail").innerHTML = "Must be a valid email";
-        return false;
+
     }
-    else{
-        document.getElementById("outputEmail").setAttribute("style","display: none");
-        return true;
+    else {
+        document.getElementById("outputEmail").setAttribute("style", "display: none");
+
     }
 }
 
-function ListClient()
-{
-var tabOfClient=JSON.parse(localStorage.getItem("tabOfClient"));
-var tab=document.getElementById("divlist");
-var list="<table id='table'>";
-for(var i=0; i<tabOfClient.length; i++){
-    list+="<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone Number</th><th>Adresse</th><th>Age</th><th>Solde</th></tr>";
-    list+="<tr><td>"+ tabOfClient[i].Firstname+"</td>";
-    list+="<td>"+tabOfClient[i].Lastname+"</td>";
-    list+="<td>"+tabOfClient[i].Email+"</td>";
-    list+="<td>"+tabOfClient[i].Tel+"</td>";
-    list+="<td>"+tabOfClient[i].Adresse+"</td>";
-    list+="<td>"+tabOfClient[i].Age+"</td>";
-    list+="<td>"+tabOfClient[i].Solde+"</td></tr>";
+
+
+
+
+function ListClient() {
+    var tabOfClient = JSON.parse(localStorage.getItem("tabOfClient"));
+    var list = '<table class="table1">';
+    list += "<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone Number</th><th>Adresse</th><th>Age</th><th>Solde</th></tr>";
+
+    for (i = 0; i < tabOfClient.length; i++) {
+
+        list += "<tr><td>" + tabOfClient[i].Firstname + "</td>";
+        list += "<td>" + tabOfClient[i].Lastname + "</td>";
+        list += "<td>" + tabOfClient[i].Email + "</td>";
+        list += "<td>" + tabOfClient[i].Tel + "</td>";
+        list += "<td>" + tabOfClient[i].Adresse + "</td>";
+        list += "<td>" + tabOfClient[i].Age + "</td>";
+        list += "<td>" + tabOfClient[i].Solde + "</td></tr>";
+    }
+    list += "</table>";
+    var divlist = document.getElementById("divlist");
+    divlist.innerHTML = list;
 }
-list+= "</table>";
-tab.innerHTML=list;
+
+
+
+
+
+function viewProfil() {
+    var tabOfClient = JSON.parse(localStorage.getItem("tabOfClient"));
+    var loggeduser = JSON.parse(localStorage.getItem("loggedId"));
+
+    for (i = 0; i < tabOfClient.length; i++) {
+        if (tabOfClient[i].Id == loggeduser)
+       { 
+       
+        client = tabOfClient[i];
+        }
+    }
+    
+    profile = " <strong>First Name:    </strong> " + client.Firstname + "<br><br>";
+    profile += "<strong>Last Name:    </strong> " + client.Lastname + "<br><br>";
+    profile += "<strong>Email:    </strong>" + client.Email + "<br><br>";
+    profile += "<strong>Phone Number:   </strong> " + client.Tel + "<br><br>";
+    profile += "<strong>Adresse:   </strong> " + client.Adresse + "<br><br>";
+    profile += "<strong>Age:    </strong> " + client.Age + "<br><br>";
+    profile += "<strong>Solde:     </strong> " + client.Solde + "<br><br>";
+    profile += "<button class='btn btn-primary' onclick='editProfil()'> Edit</button>";
+    var profil = document.getElementById("pro");
+    profil.innerHTML = profile;
+}
+/* pas encore terminée*/
+function editProfil(){
+
+    var profil = document.getElementById("pro");
+    var edit= document.getElementById("edit");
+    edit.innerHTML=profil.innerHTML;
+    document.getElementById("edit").readonly=false;
 }
--- login.js
+++ login.js
@@ -1,22 +1,21 @@
 function log() {
     var usermail = document.getElementById("customer-email").value;
     var userpwd = document.getElementById("customer-password").value;
-    var users, loggedusers = [];
+    var loggedusers = [];
     console.log(usermail);
     console.log(userpwd);
-    users = JSON.parse(localStorage.getItem('tabOfClient'));
+    var users = JSON.parse(localStorage.getItem('tabOfClient'));
     for (var i = 0; i < users.length; i++) {
         if ((usermail == users[i].Email) && (userpwd == users[i].Password)) {
             alert("welcome" + " "+users[i].Firstname);
             var logged = {
-                id: i,
+                id: users[i].Id,
                 mail: usermail,
                 password: userpwd
             }
             loggedusers.push(logged);
-            console.log(loggedusers);
             localStorage.setItem('loggeduser', JSON.stringify(loggedusers));
-            window.location.href = "watches.html";
+            window.location.href = "HomeClient.html";
             break;
         } else if (((usermail == users[i].email) && (userpwd == users[i].Password)) && userpwd  == "admin") {
             alert("welcome" + users[i].Firstname);
@@ -34,4 +33,13 @@ function log() {
         alert("please create an account!");
     }
     
+}
+
+
+function logout() {
+   
+    loggedusers = JSON.parse(localStorage.getItem('loggeduser'));        
+        localStorage.removeItem("loggeduser");
+        document.location.href = "watches.html";
+ 
 }
